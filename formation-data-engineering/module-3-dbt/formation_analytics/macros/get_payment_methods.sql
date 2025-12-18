3db82dac39f4f5161ab7ec8e4e576adb2cea2189
Antoine PERRY <antoine.ap.57@gmail.com>
2025-12-18 08:14:44 +0100

    update

--- formation-data-engineering/module-3-dbt/formation_analytics/macros/get_payment_methods.sql
+++
@@ -1,8 +0,0 @@
--- ============================================================================
--- MACRO: get_payment_methods
--- Retourne la liste des méthodes de paiement valides
--- ============================================================================
-
-{% macro get_payment_methods() %}
-    {{ return(['credit_card', 'debit_card', 'paypal', 'bank_transfer', 'cash', 'crypto']) }}
-{% endmacro %}
